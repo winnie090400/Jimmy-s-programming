40e49cfe3cca1bd8bbc6484a478f6e9be344bdfc
born1031 <51741331+born1031@users.noreply.github.com>
2019-06-18 11:24:34 +0800

    update

--- Java_Hello/src/Lab_Test/test.java
+++ Java_Hello/src/Lab_Test/test.java
@@ -4,10 +4,10 @@ package Lab_Test;
 public class test {
 
     public static void main(String[] args) {
-//        int result = 0;
-//        for(int i=1; i<=100; i++){
-//            result += i;
-//            System.out.println(result);
-//        }
+        int result = 0;
+        for(int i=1; i<=100; i++){
+            result += i;
+            System.out.println(result);
+        }
     }
 }
